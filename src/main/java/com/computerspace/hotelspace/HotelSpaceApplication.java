04abcc507dc22bf1b558452cebe62b4d27727604
luislopez1981 <luislopez1981@hotmail.com>
2018-05-07 09:55:39 +0200

    first commit

---
+++ src/main/java/com/computerspace/hotelspace/HotelSpaceApplication.java
@@ -0,0 +1,12 @@
+package com.computerspace.hotelspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSpaceApplication.class, args);
+	}
+}
